272e1a5a21b1708dafce71163ca6a7ec8940b15f
Banhbaosua <116063527+Banhbaosua@users.noreply.github.com>
2024-08-10 01:25:24 +0700

    Revert "Add Exp system"

--- Assets/Script/ScriptableObjectScript/Variable/FloatSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "FloatVariable", menuName = "SOVariable/FloatVariable")]
-public class FloatSO : SOVariable<float>
-{
-    
-}
