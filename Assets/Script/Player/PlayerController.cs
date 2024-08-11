6df05d94c603f5a34be60f253a6449a14c88bd7d
Banhbaosua <angisita12345@gmail.com>
2024-08-11 20:53:39 +0700

    fix movement

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using Unity.VisualScripting;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] PlayerInputCtl playerInputCtl;
     [SerializeField] CharacterController characterController;
     [SerializeField] float moveSpeed;
+    private float velocity;
+    private float gravity = -Physics.gravity.magnitude;
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
@@ -27,14 +30,18 @@ public class PlayerController : MonoBehaviour
         Vector2 playerPos = new Vector2(transform.position.x, transform.position.z);
         var newPos = playerPos + playerInputCtl.GetTouchDirection();
         var direction2D = newPos - playerPos;
-        var direction3D = new Vector3(direction2D.x, 0, direction2D.y);
+        velocity = gravity * Time.deltaTime;
+        var direction3D = new Vector3(direction2D.x, velocity, direction2D.y);
         characterController.Move(moveSpeed * Time.deltaTime * direction3D.normalized);
         LookHandle(direction3D.normalized);
     }
 
     void LookHandle(Vector3 direction)
     {
-        if(direction != Vector3.zero)
-            this.transform.rotation = Quaternion.LookRotation(direction);
+        if (direction != Vector3.zero)
+        {
+            direction.y = 0;
+            this.transform.forward = direction;
+        }
     }
 }
